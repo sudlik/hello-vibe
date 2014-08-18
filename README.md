11cd9473d387ff35079e268025ec79fc6a8787eb
Marek "Sudlik" Sudoł <sudlik@sudlik.pl>
2014-08-18 19:55:44 +0200

    enable multi-threading

--- README.md
+++ README.md
@@ -11,4 +11,8 @@ Usage
 ------------
 1. `$ git clone git@github.com:sudlik/hello-vibe.git`
 2. `$ cd hello-vibe`
-3. `dub run`
+3. `$ dub run`
+
+Test
+------------
+`$ ab -n 100000 -c 200 -k http://127.0.0.1:8080/`
--- source/app.d
+++ source/app.d
@@ -3,8 +3,12 @@ import vibe.d;
 shared static this()
 {
 	auto settings = new HTTPServerSettings;
+    
+    settings.options |= HTTPServerOption.distribute;
 	settings.port = 8080;
 	settings.bindAddresses = ["::1", "127.0.0.1"];
+
+    enableWorkerThreads();
 	listenHTTP(settings, &hello);
 
 	logInfo("Please open http://127.0.0.1:8080/ in your browser.");
